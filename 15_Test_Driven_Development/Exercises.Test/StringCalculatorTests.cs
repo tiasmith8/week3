2bee9e1b4c01976e935570670a8bd466aec91f13
Ken Bohac <bohac91@gmail.com>
2019-02-01 15:32:03 -0500

    2nd TDD cycle done

--- 15_Test_Driven_Development/Exercises.Test/StringCalculatorTests.cs
+++ 15_Test_Driven_Development/Exercises.Test/StringCalculatorTests.cs
@@ -6,20 +6,38 @@ namespace Exercises.Test
     [TestClass]
     public class StringCalculatorTests
     {
+        private const string Numbers = "";
+
         [TestMethod]
-        public void Add(string numbers)
+        public void StringCalculator_AddTestSendEmptyStringReceive0()
         {
             //Arrange  declare and intialize new string containing 2 numbers
             //instantiate a new object from the StringCalculator Class
             StringCalculator testAdd = new StringCalculator();
-
+            string input = "";
 
             //Act declare and initliaze new int variable equal to Add(string numbers) 
-            int result = testAdd.Add("");
+            int result = testAdd.Add(input);
             //Assert
             //compare result of Add(string numbers) actual result with expected result
             //evaluate equality to detrmine if method .Add works. 
             Assert.AreEqual(0, result, "empty string returns 0");
         }
+
+        [TestMethod]
+        public void StringCalculator_AddTestWith1Returns1()
+        {
+            //Arrange
+            StringCalculator unitTest = new StringCalculator();
+
+            //Act - public int Add(string numbers)
+            int actualValueReturned = unitTest.Add("1");
+
+            //Assert
+            Assert.AreEqual(1, actualValueReturned, "Sending string of 1 returns int 1.");
+
+        }
+
+
     }
 }
--- 15_Test_Driven_Development/Exercises/StringCalculator.cs
+++ 15_Test_Driven_Development/Exercises/StringCalculator.cs
@@ -4,7 +4,50 @@ using System.Text;
 
 namespace Exercises
 {
-    class StringCalculator
+    public class StringCalculator
     {
+        public int Add(string numbers)
+        {
+            
+            if (numbers == "")
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+            
+
+            //take string numbers
+            //split it into new array
+
+            //int sum = 0;
+            //if (numbers.Length > 1)
+            //{
+            //    string[] numbersBrokenOut = numbers.Split(',');
+
+            //    // loop resulting string from split
+            //    foreach (string element in numbersBrokenOut)
+            //    {
+
+
+
+            //        sum += int.Parse(element);
+            //    }
+
+            //}
+            //else
+            //{
+            //    sum = int.Parse(numbers);
+            //}
+
+            ////return output
+            //return sum;
+            
+        }
+
+
+
     }
 }
