c5fe2bfe7a9a77e5c51b4e124313343f3bd4a5f3
Tia Smith <tia.smith@gmail.com>
2019-01-31 20:43:10 -0500

    merge error

---
+++ 11_Inheritance/BankTellerExerciseTests/Classes/BankAccountTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankTellerExercise.Classes;
+
+namespace BankTellerExerciseTests.Classes
+{
+    [TestClass]
+    public class BankAccountTests
+    {
+        [TestMethod]
+        public void TestNegativeDepositAmount()
+        {
+            //Arrange
+            BankAccount bankAccount = new BankAccount();
+
+            //Act - deposit negative amount
+            decimal returnedBalance = bankAccount.Deposit(-100);
+
+            //Assert - balance should remain the same
+            Assert.AreEqual(0, (double)returnedBalance, "Balance should be unchanged.");
+
+        }
+
+    }
+}
