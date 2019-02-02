1f75f2a9460665601e5937415e28a60f181590bd
Tia Smith <tia.smith@gmail.com>
2019-02-02 18:25:34 -0500

    Add test method Convert_1_Digit_Number_0_To_Zero

---
+++ 15_Test_Driven_Development/Exercises.Test/NumbersToWordsTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercises.Test
+{
+    [TestClass]
+    public class NumbersToWordsTests
+    {
+        [TestMethod]
+        public void Convert_1_Digit_Number_0_To_Zero()
+        {
+            //Arrange
+            NumbersToWords numsToWords = new NumbersToWords();
+
+            //Act
+            string word = numsToWords.Convert(0);
+
+            //Assert
+            Assert.AreEqual("zero", word, "0 returns zero");
+        }
+
+
+
+    }
+}
