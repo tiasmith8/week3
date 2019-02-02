68ca300b66b0984b7d9a6fdc07fcdfab61eed986
Tia Smith <tia.smith@gmail.com>
2019-02-02 18:27:29 -0500

    Add passing code for test method Convert_1_Digit_Number_0_To_Zero

---
+++ 15_Test_Driven_Development/Exercises/NumbersToWords.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercises
+{
+    public class NumbersToWords
+    {
+        public string Convert(int number)
+        {
+
+            return "zero";
+        }
+
+
+
+    }
+}
