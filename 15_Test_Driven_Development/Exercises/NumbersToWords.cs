1eedd30ed5c93e76db20b7662813d821d2ee9b00
Tia Smith <tia.smith@gmail.com>
2019-02-02 18:38:48 -0500

    Add passing code for test method Convert_Single_Digit_Numbers_To_Words

--- 15_Test_Driven_Development/Exercises/NumbersToWords.cs
+++ 15_Test_Driven_Development/Exercises/NumbersToWords.cs
@@ -8,7 +8,25 @@ namespace Exercises
     {
         public string Convert(int number)
         {
-            return "zero";
+            //string wordToReturn = "zero";
+
+            //Convert single digit numbers to words
+            Dictionary<int, string> dictNumsToWords = new Dictionary<int, string>();
+
+            //Add values for 0-10
+            dictNumsToWords.Add(0, "zero");
+            dictNumsToWords.Add(1, "one");
+            dictNumsToWords.Add(2, "two");
+            dictNumsToWords.Add(3, "three");
+            dictNumsToWords.Add(4, "four");
+            dictNumsToWords.Add(5, "five");
+            dictNumsToWords.Add(6, "six");
+            dictNumsToWords.Add(7, "seven");
+            dictNumsToWords.Add(8, "eight");
+            dictNumsToWords.Add(9, "nine");
+            dictNumsToWords.Add(10, "ten");
+
+            return dictNumsToWords[number];
         }
 
 
