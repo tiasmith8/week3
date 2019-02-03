b8ada7b0bb3a6163d7f387ce38c67128e568968f
Tia Smith <tia.smith@gmail.com>
2019-02-03 00:04:48 -0500

    Add test method SendingZeroReturns0

---
+++ 15_Test_Driven_Development/Exercises.Test/WordsToNumbersTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercises.Test
+{
+    [TestClass]
+    class WordsToNumbersTest
+    {
+        [TestMethod]
+        public void SendingZeroReturns0()
+        {
+            WordsToNumbers zeroConversion = new WordsToNumbers();
+            int actual = zeroConversion.Convert("zero");
+            Assert.AreEqual(0, actual, "zero returns 0");
+            
+        }
+
+    }
+}
