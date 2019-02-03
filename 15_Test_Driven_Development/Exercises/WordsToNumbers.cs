ddde5c370619a91ec430eafe98b6ebdff7ed1e0f
Tia Smith <tia.smith@gmail.com>
2019-02-03 00:07:40 -0500

    Add passing code for test method SendingZeroReturns0

---
+++ 15_Test_Driven_Development/Exercises/WordsToNumbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercises
+{
+    public class WordsToNumbers
+    {
+        public int Convert(string number)
+        {
+
+            return 0;
+
+        }
+
+
+
+    }
+}
